da898c666b8514736fae3f22bcdbdcd3058f61dc
Sirerow <sokolovskiy3006@gmail.com>
2024-10-21 18:27:08 +0300

    основные методы переключения фильтрации

---
+++ adminPanelMethods.py
@@ -0,0 +1,11 @@
+from maskMethods import Masking
+from db import DaBa
+
+def changeMaskMethod(maskMethodType):
+    Masking.changeMaskType(type)
+
+def addRegularExpression(Expression):
+    pass
+    DaBa().saveInfoInRegular(Expression)
+#экспрешн должен попадать в бд
+#саму бд не смог создать
