8629168224f32aa28a6efed7116c777728fcc70d
Goose <Kolosovich19@gmail.com>
2024-10-23 00:15:06 +0300

    новая бд

--- models/UserInfo.py
+++ models/UserInfo.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
-
+from datetime import  datetime
 
 @dataclass
 class UserInfo:
     userInfoId: int
     userId: int
     secretInfo: str
+    endpoint: str
+    timestamp: datetime
 
 
