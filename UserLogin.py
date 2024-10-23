1654494383a4e564152bf8966f1832fc7ab0877e
Goose <Kolosovich19@gmail.com>
2024-10-24 00:22:54 +0300

    пофикшено нахой

--- UserLogin.py
+++ UserLogin.py
@@ -1,24 +1,41 @@
-class UserLogin:
-    async def fromBD(self, userID, DB):
-        self.__user = await DB.getUserByID(userID)
-        self.is_authenticated = True
-        return self
+import asyncio
+from flask_login import UserMixin
+
+
+async def fromBD(userID: int, DB):
+    """Асинхронное получение пользователя из базы данных"""
+    __user = await DB.getUserByID(userID)
+    return __user
+
+class UserLogin(UserMixin):
+    def __init__(self):
+        self.__user = None
 
     def get_user(self):
+        """Возвращает текущего пользователя"""
         return self.__user
 
-    async def createUser(self, user):
+    async def createUser(self, user) -> 'UserLogin':
+        """Создание нового пользователя"""
         self.__user = user
         return self
 
-    async def is_authenticated(self):
+
+
+    async def is_authenticated(self) -> bool:
+        """Проверка, аутентифицирован ли пользователь"""
+        return self.__user is not None
+
+    def is_active(self) -> bool:
+        """Проверка, активен ли пользователь"""
         return True
 
-    def is_active(self):
-        return True
+    def is_anonymous(self) -> bool:
+        """Проверка, является ли пользователь анонимным"""
+        return self.__user is None
 
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return str(self.__user.adminId)
+    def get_id(self) -> str:
+        """Получение идентификатора пользователя"""
+        if self.__user is not None:
+            return str(self.__user.adminId)
+        return None
